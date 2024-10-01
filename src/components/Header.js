987a456bcced83395e81b1f8b680d0fb4b01813f
karolsobon <karolsobon@aol.com>
2024-10-01 21:43:34 +0200

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
-import img1 from '../resources/Agua-natural.mp4';
+import video1 from '../resources/video1.mp4';
+import './Header.css'; // Upewnij się, że ścieżka do pliku CSS jest poprawna
+
 
 const Header = () => {
   return (
     <Carousel fade controls={true} indicators={true}>
       <Carousel.Item>
-        <img className="d-block w-100 header-image" src={img1} alt="First slide" />
+        <video className="d-block w-100 header-video" src={video1} autoPlay loop muted />
+        <div className="carousel-caption d-flex flex-column justify-content-center align-items-center h-100">
+          <div className="bg-overlay">
+          <h1 className="display-1 text-uppercase text-with-border p-3">Think OZE</h1>
+          <p className="lead p-2">Twoje wsparcie w kompletowaniu zestawów PV</p>
+          </div>
+        </div>
       </Carousel.Item>
     </Carousel>
   );
