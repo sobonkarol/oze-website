c62a2f4481b4793e65c6d9d74da98e6ab50ef043
karolsobon <karolsobon@aol.com>
2024-10-15 10:23:39 +0200

    zapobieganie filmu

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <Carousel fade controls={true} indicators={true}>
       <Carousel.Item className='carousel-item'>
-        <video className="d-block w-100 header-video" src={video1} autoPlay loop muted />
+        <video className="d-block w-100 header-video" src={video1} autoPlay loop muted playsInline />
         <div className="carousel-caption d-flex flex-column justify-content-center align-items-center h-100">
           <div className="bg-overlay">
           <h1 className="display-1 text-uppercase text-with-border p-3">Think OZE</h1>
